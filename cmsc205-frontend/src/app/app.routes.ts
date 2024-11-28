bab77b9947c2a63bbbe0c625ac566025de9cccf7
Vincent Flores <94419368+flores-vincentangelo@users.noreply.github.com>
2024-11-28 23:30:38 +0800

    progress

--- cmsc205-frontend/src/app/app.routes.ts
+++ cmsc205-frontend/src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 
 export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/map' },
   { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.routes').then(m => m.WELCOME_ROUTES) },
   { path: '', component: HomePageComponent, title: 'Home',
     children: [
