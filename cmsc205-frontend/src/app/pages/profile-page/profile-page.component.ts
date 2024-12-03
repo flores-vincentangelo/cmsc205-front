21553f9f421fc14dd4462f9c41d725424ebfaa73
Vincent Flores <94419368+flores-vincentangelo@users.noreply.github.com>
2024-12-03 18:43:33 +0800

    profile page updates

--- cmsc205-frontend/src/app/pages/profile-page/profile-page.component.ts
+++ cmsc205-frontend/src/app/pages/profile-page/profile-page.component.ts
@@ -1,12 +1,89 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  AbstractControl,
+  NonNullableFormBuilder,
+  ReactiveFormsModule,
+  ValidationErrors,
+  Validators,
+  ValidatorFn
+} from '@angular/forms';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
 
 @Component({
   standalone: true,
   selector: 'app-profile-page',
-  imports: [],
+  imports: [ReactiveFormsModule, NzButtonModule, NzCheckboxModule, NzFormModule, NzInputModule],
   templateUrl: './profile-page.component.html',
   styleUrl: './profile-page.component.css'
 })
 export class ProfilePageComponent {
 
+  formLabelSm: number = 10;
+  formLabelXs: number = 24;
+  formControlSm: number = 14;
+  registerOffset: number = 0
+  private destroy$ = new Subject<void>();
+
+  private fb = inject(NonNullableFormBuilder);
+  validateForm = this.fb.group({
+    email: this.fb.control('', [Validators.email, Validators.required]),
+    password: this.fb.control('', [Validators.required]),
+    checkPassword: this.fb.control('', [Validators.required, this.passwordMatchValidator()]),
+    firstname: this.fb.control('', [Validators.required]),
+    lastname: this.fb.control('', [Validators.required]),
+    agree: this.fb.control(false)
+  });
+
+  public screenWidth: any;
+  public screenHeight: any;
+
+  constructor() {
+  }
+
+
+  ngOnInit(): void {
+    this.screenWidth = window.innerWidth -1;
+    this.screenHeight = window.innerHeight;
+    this.registerOffset = this.screenWidth >= 576 ? this.formLabelSm : 0;
+
+    this.validateForm.controls['password'].valueChanges.pipe(takeUntil(this.destroy$)).subscribe(() => {
+      this.validateForm.controls['checkPassword'].updateValueAndValidity();
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  submitForm(): void {
+    if (this.validateForm.valid) {
+      console.log(this.validateForm.value)
+
+    } else {
+      Object.values(this.validateForm.controls).forEach(control => {
+        if (control.invalid) {
+          control.markAsDirty();
+          control.updateValueAndValidity({ onlySelf: true });
+        }
+      });
+    }
+  }
+
+  passwordMatchValidator(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      if (!control.value) {
+        return { required: true };
+      } else if (control.value !== this.validateForm.controls['password'].value) {
+        return { confirm: true, error: true };
+      }
+      return {};
+    }
+  }
 }
