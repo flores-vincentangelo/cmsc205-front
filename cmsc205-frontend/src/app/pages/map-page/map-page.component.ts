1bc9ecf8a8b50110e2edc4f50ed3ea17bfbdf6be
Vincent Flores <94419368+flores-vincentangelo@users.noreply.github.com>
2024-12-04 14:36:20 +0800

    googlemaps integration

--- cmsc205-frontend/src/app/pages/map-page/map-page.component.ts
+++ cmsc205-frontend/src/app/pages/map-page/map-page.component.ts
@@ -1,12 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { GoogleMapsModule } from '@angular/google-maps';
 
 @Component({
   standalone: true,
   selector: 'app-map-page',
-  imports: [],
+  imports: [CommonModule, GoogleMapsModule],
   templateUrl: './map-page.component.html',
   styleUrl: './map-page.component.css'
 })
 export class MapPageComponent {
-
+  center: google.maps.LatLngLiteral = { lat: 40.73061, lng: -73.935242 }; // Example: New York City
+  zoom = 12;
 }
