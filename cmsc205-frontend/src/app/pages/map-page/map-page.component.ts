3ec73a7dc4bcebbf6f8d10f397f976a83b7b0ac0
Vincent Flores <94419368+flores-vincentangelo@users.noreply.github.com>
2024-12-07 22:02:07 +0800

    google maps centering at current location

--- cmsc205-frontend/src/app/pages/map-page/map-page.component.ts
+++ cmsc205-frontend/src/app/pages/map-page/map-page.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { GoogleMapsModule } from '@angular/google-maps';
+import { LocationService } from '../../services/location.service';
 
 @Component({
   standalone: true,
@@ -10,6 +11,21 @@ import { GoogleMapsModule } from '@angular/google-maps';
   styleUrl: './map-page.component.css'
 })
 export class MapPageComponent {
+  ls = inject(LocationService)
+
+  mapOptions: google.maps.MapOptions = {
+    center: { lat: 40.73061, lng: -73.935242 },
+    zoom: 15
+  }
   center: google.maps.LatLngLiteral = { lat: 40.73061, lng: -73.935242 }; // Example: New York City
-  zoom = 12;
+  zoom = 15;
+
+  ngOnInit(): void {
+    this.ls.getLocation()
+    this.ls.currentLoc$.subscribe(value => {
+      this.center = { lat: value.lat, lng: value.lng };
+      this.mapOptions.center = { lat: value.lat, lng: value.lng };
+    })
+  }
+
 }
---
+++ cmsc205-frontend/src/app/services/location.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Coordinates } from '../models/coordinates';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LocationService {
+
+  currentLoc$ = new BehaviorSubject<Coordinates>({lat:0, lng: 0});
+  constructor() { }
+
+  getLocation(): void{
+       navigator.geolocation.getCurrentPosition((position: GeolocationPosition) => {
+        if (position) {
+          this.currentLoc$.next({
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          })
+        } else {
+        }
+      })
+  }
+}
