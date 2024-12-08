f30c9c22fb24ea92b9a1698bcc0f6f96595c107c
Vincent Flores <94419368+flores-vincentangelo@users.noreply.github.com>
2024-12-08 18:20:09 +0800

    info window works

---
+++ cmsc205-frontend/src/app/components/map/marker/marker.component.ts
@@ -0,0 +1,32 @@
+import { Component, EventEmitter, Input, Output, ViewChild } from '@angular/core';
+import { MapAdvancedMarker, MapInfoWindow } from '@angular/google-maps';
+
+@Component({
+  selector: 'app-marker',
+  standalone: true,
+  imports: [MapAdvancedMarker, MapInfoWindow],
+  templateUrl: './marker.component.html',
+  styleUrl: './marker.component.css'
+})
+export class MarkerComponent {
+  @Input() position!: google.maps.LatLngLiteral;
+  @Output() openInfoWindowEvent = new EventEmitter<any>();
+
+  markerOptions: google.maps.marker.AdvancedMarkerElementOptions = {
+    gmpDraggable: false,
+    gmpClickable: true,
+  };
+
+  clickMarker(position: google.maps.LatLngLiteral) {
+    console.log(position)
+  }
+
+  openInfoWindow(marker: MapAdvancedMarker) {
+    const emitObj = {
+      marker: marker,
+      position: this.position
+    }
+    this.openInfoWindowEvent.emit(emitObj)
+  }
+
+}
--- cmsc205-frontend/src/app/pages/map-page/map-page.component.ts
+++ cmsc205-frontend/src/app/pages/map-page/map-page.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule, JsonPipe } from '@angular/common';
 import { Component, inject, ViewChild } from '@angular/core';
-import { GoogleMap, MapAdvancedMarker, MapMarker } from '@angular/google-maps';
+import { GoogleMap, MapInfoWindow, MapAdvancedMarker } from '@angular/google-maps';
 import { LocationService } from '../../services/location.service';
+import { MarkerComponent } from '../../components/map/marker/marker.component';
 
 @Component({
   standalone: true,
   selector: 'app-map-page',
-  imports: [CommonModule, GoogleMap, MapAdvancedMarker, JsonPipe],
+  imports: [CommonModule, GoogleMap, JsonPipe, MarkerComponent, MapInfoWindow],
   templateUrl: './map-page.component.html',
   styleUrl: './map-page.component.css',
 })
@@ -28,11 +29,12 @@ export class MapPageComponent {
     glyphColor: 'blue',
     scale: .5,
   }
-  markerOptions: google.maps.marker.AdvancedMarkerElementOptions = {
-    gmpDraggable: false,
-    gmpClickable: true,
-  };
+
   markerPositions: google.maps.LatLngLiteral[] = [];
+
+  @ViewChild(MapInfoWindow) infoWindow!: MapInfoWindow;
+  infoWindowContents: string = '';
+
 
   async ngOnInit(): Promise<void> {
     // <svg xmlns="http://www.w3.org/2000/svg" fill="#000000" width="800px" height="800px" viewBox="0 0 32 32" version="1.1"><path d="M4 12q0-3.264 1.6-6.016t4.384-4.352 6.016-1.632 6.016 1.632 4.384 4.352 1.6 6.016q0 1.376-0.672 3.2t-1.696 3.68-2.336 3.776-2.56 3.584-2.336 2.944-1.728 2.080l-0.672 0.736q-0.256-0.256-0.672-0.768t-1.696-2.016-2.368-3.008-2.528-3.52-2.368-3.84-1.696-3.616-0.672-3.232zM8 12q0 3.328 2.336 5.664t5.664 2.336 5.664-2.336 2.336-5.664-2.336-5.632-5.664-2.368-5.664 2.368-2.336 5.632z"/></svg>
@@ -77,8 +79,8 @@ export class MapPageComponent {
     }
   }
 
-  clickMarker(position: google.maps.LatLngLiteral) {
-    console.log(position)
+  openInfoWindow(object: any) {
+    this.infoWindowContents = `${JSON.stringify(object.position)}`
+    this.infoWindow.open(object.marker);
   }
-
 }
