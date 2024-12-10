777f4df995140908aa932ae8faccbfed7fba90d8
Vincent Flores <94419368+flores-vincentangelo@users.noreply.github.com>
2024-12-10 20:10:12 +0800

    knowledge

--- cmsc205-frontend/src/app/pages/knowledge-page/knowledge-page.component.ts
+++ cmsc205-frontend/src/app/pages/knowledge-page/knowledge-page.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { NzListModule } from 'ng-zorro-antd/list';
+
+import { NzTypographyModule } from 'ng-zorro-antd/typography';
 
 @Component({
   standalone: true,
   selector: 'app-knowledge-page',
-  imports: [],
+
+  imports: [NzTypographyModule, NzListModule],
   templateUrl: './knowledge-page.component.html',
   styleUrl: './knowledge-page.component.css',
 })
-export class KnowledgePageComponent {}
+export class KnowledgePageComponent {
+  sanitation: string[] = ['string1', 'string2', 'string3'];
+  conservation: string[] = ['string1', 'string2', 'string3'];
+}
